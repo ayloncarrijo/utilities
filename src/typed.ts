f0d8e2d4ec452e38d26088810f24954914eac7bf
Aylon Carrijo <aylon.gabriel@outlook.com>
2023-03-21 03:44:48 -0300

    feat(typed): add type guard utilities

---
+++ src/typed.ts
@@ -0,0 +1,34 @@
+import type { Primitive } from "./types";
+
+export const isObject = (value: unknown): value is object =>
+  value != null && typeof value === "object";
+
+export const isPlainObject = (
+  value: unknown
+): value is Record<string, unknown> =>
+  value != null && value.constructor === Object;
+
+export const isString = (value: unknown): value is string =>
+  typeof value === "string";
+
+export const isNumber = (value: unknown): value is number =>
+  typeof value === "number" && !isNaN(value);
+
+export const isArray = Array.isArray as (
+  value: unknown
+) => value is Array<unknown>;
+
+export const isInt = (value: unknown): value is number =>
+  isNumber(value) && value % 1 === 0;
+
+export const isFloat = (value: unknown): value is number =>
+  isNumber(value) && value % 1 !== 0;
+
+export const isSymbol = (value: unknown): value is symbol =>
+  typeof value === "symbol";
+
+export const isDate = (value: unknown): value is Date =>
+  value instanceof Date && !isNaN(value.getTime());
+
+export const isPrimitive = (value: unknown): value is Primitive =>
+  value == null || (typeof value !== "object" && typeof value !== "function");
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export type Primitive =
+  | string
+  | number
+  | bigint
+  | boolean
+  | symbol
+  | undefined
+  | null;
